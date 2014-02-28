0f2ab70cef42bfaae63e77f6753cde130297a09a
Francisco A. Lozano <flozano@gmail.com>
2014-02-28 19:56:51 +0100

    Initial commit

---
+++ src/main/java/com/flozano/jacksonplayground/Animal.java
@@ -0,0 +1,75 @@
+package com.flozano.jacksonplayground;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+public class Animal {
+
+	/** Domestic animals **/
+
+	@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "behaves", visible = true)
+	@JsonSubTypes({ @JsonSubTypes.Type(value = Dog.class, name = "NICE"),
+			@JsonSubTypes.Type(value = Cat.class, name = "NASTY") })
+	public static class DomesticAnimal extends Animal {
+
+	}
+
+	public static class Dog extends DomesticAnimal {
+		public String dogName;
+
+		public String getDogName() {
+			return dogName;
+		}
+
+		public Dog(String dogName) {
+			this.dogName = dogName;
+		}
+	}
+
+	public static class Cat extends DomesticAnimal {
+		public String catName;
+
+		public String getCatName() {
+			return catName;
+		}
+
+		public Cat(String catName) {
+			this.catName = catName;
+		}
+
+	}
+
+	/** Wild animals **/
+	@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "behaves", visible = true)
+	@JsonSubTypes({ @JsonSubTypes.Type(value = Kangaroo.class, name = "NICE"),
+			@JsonSubTypes.Type(value = Snake.class, name = "NASTY") })
+	public static class WildAnimal extends Animal {
+
+	}
+
+	public static class Snake extends WildAnimal {
+		public String snakeName;
+
+		public String getSnakeName() {
+			return snakeName;
+		}
+
+		public Snake(String snakeName) {
+			this.snakeName = snakeName;
+		}
+
+	}
+
+	public static class Kangaroo extends WildAnimal {
+		public String kangarooName;
+
+		public String getKangarooName() {
+			return kangarooName;
+		}
+
+		public Kangaroo(String kangarooName) {
+			this.kangarooName = kangarooName;
+		}
+
+	}
+}
---
+++ src/test/java/com/flozano/jacksonplayground/HierTest.java
@@ -0,0 +1,63 @@
+package com.flozano.jacksonplayground;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flozano.jacksonplayground.Animal.Cat;
+import com.flozano.jacksonplayground.Animal.Dog;
+import com.flozano.jacksonplayground.Animal.Kangaroo;
+import com.flozano.jacksonplayground.Animal.Snake;
+
+public class HierTest {
+	// quite silly data model...
+	final Kangaroo kangaroo = new Kangaroo("Bob");
+	final Snake snake = new Snake("Lisa");
+	final Dog dog = new Dog("Maria");
+	final Cat cat = new Cat("Esther");
+
+	ObjectMapper mapper;
+
+	@Before
+	public void setUp() {
+		mapper = new ObjectMapper();
+	}
+
+	@Test
+	public void testSerializationOneByOne() throws JsonProcessingException {
+		JSONObject obj = new JSONObject(mapper.writeValueAsString(snake));
+		System.err.println(obj);
+		assertEquals(snake.getSnakeName(), obj.getString("snakeName"));
+		assertEquals("NASTY", obj.getString("behaves"));
+	}
+
+	@Test
+	public void testSerializationInPlainCollection() throws JSONException,
+			JsonProcessingException {
+		JSONArray arr = new JSONArray(mapper.writeValueAsString(Arrays
+				.<Animal> asList(snake, kangaroo, dog, cat)));
+		System.err.println(arr);
+		for (int i = 0; i < arr.length(); i++) {
+			assertNotNull(arr.getJSONObject(i).optString("behaves", null));
+		}
+	}
+
+	@Test
+	public void testSerializationInPlainCollectionWithHelp()
+			throws JSONException, JsonProcessingException {
+		JSONArray arr = new JSONArray(mapper.writerWithType(
+				mapper.getTypeFactory().constructCollectionType(List.class,
+						Animal.class)).writeValueAsString(
+				Arrays.<Animal> asList(snake, kangaroo, dog, cat)));
+		System.err.println(arr);
+	}
+}
